6e1168160ecc7aa650c85f5ecd114ef0956eb7ab
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2025-02-22 19:33:31 -0300

    services

---
+++ cart-service/src/test/java/com/gayatech/cart_service/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gayatech.cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
